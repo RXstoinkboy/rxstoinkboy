bef0c935911ad4208a9e9b704eff0f19175389d8
RXstoinkboy <erykludwin@gmail.com>
2018-12-01 15:46:02 +0100

    build complete. NEXT test if everything is working correctly

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: {
+    index: './src/js/index.mjs',
+  },
+  output: {
+    path: `${__dirname}/dist/js`,
+    filename: 'bundle-[name].mjs'
+  },
+  watch: true,
+  mode: 'development', // ta opcja zostanie pominięta jeżeli użyjemy npm run build
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['env', {
+              targets: {
+                browsers: ['> 1%']
+              }
+            }]]
+          }
+        }
+      }
+    ]
+  }
+}
