b0d31a614f09be6d85a2b0b068fef1b40e18fff6
RXstoinkboy <erykludwin@gmail.com>
2018-12-22 19:45:58 +0100

    added scrolling to static nav. NEXT: do the same for mobile

--- src/js/skipToSection.js
+++ src/js/skipToSection.js
@@ -1,12 +1,24 @@
 export function skipToSection(e) {
-  console.log(e.target.dataset);
-  const el = {};
-  el.type = e.target.dataset.nav;
-  console.log(el.type);
+  const projects = document.querySelector('.projects');
+  const about = document.querySelector('.about');
+  const header = document.querySelector('.header');
+  const contact = document.querySelector('.contact');
 
-  el[type].scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
-  },
-  );
+  if (this.dataset.nav === 'projects') {
+    projects.scrollIntoView({
+      behavior: 'smooth', block: 'start',
+    });
+  } else if (this.dataset.nav === 'about') {
+    about.scrollIntoView({
+      behavior: 'smooth', block: 'start',
+    });
+  } else if (this.dataset.nav === 'header') {
+    header.scrollIntoView({
+      behavior: 'smooth', block: 'start',
+    });
+  } else if (this.dataset.nav === 'contact') {
+    contact.scrollIntoView({
+      behavior: 'smooth', block: 'start',
+    });
+  }
 }
