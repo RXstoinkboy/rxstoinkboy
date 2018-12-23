8a809912cf407d8b8ac963b85976cc42287142d0
RXstoinkboy <erykludwin@gmail.com>
2018-12-23 11:13:41 +0100

    added animation to projects. Have to prevent hover on it before they are active

--- src/js/stateOnScroll.js
+++ src/js/stateOnScroll.js
@@ -1,8 +1,9 @@
 export function stateOnScroll() {
   const titles = document.querySelectorAll('.title');
-  const projects = document.querySelectorAll('.container__project');
+  const projects = document.querySelectorAll('.project');
 
   titles.forEach((title) => {
+
     // half way through the item
     const itemMidY = (window.scrollY + window.innerHeight) - title.getBoundingClientRect().height / 2;
     // bottom of the item
@@ -18,6 +19,20 @@ export function stateOnScroll() {
     // else {
     //   title.classList.remove('title--active');
     // }
+
+    projects.forEach((project) => {
+      const itemMidY = (window.scrollY + window.innerHeight) - project.getBoundingClientRect().height / 2;
+      // bottom of the item
+      const itemBottom = project.offsetTop + project.getBoundingClientRect().height;
+      const isHalfShown = itemMidY > itemBottom;
+      const notScrolledPast = window.scrollY < itemBottom;
+
+      console.log(window.scrollY, { itemBottom });
+      // logic
+      if (isHalfShown && notScrolledPast) {
+        project.classList.add('project--active');
+      }
+    });
 
 
   });
