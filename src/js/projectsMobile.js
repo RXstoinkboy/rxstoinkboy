89a992fbfc9d3638bbb1543f9e45c35bde0e4734
RXstoinkboy <erykludwin@gmail.com>
2019-01-13 23:02:57 +0100

    added survey-form to projects. Updated this portfolio with classes and ids required for FCC

--- src/js/projectsMobile.js
+++ src/js/projectsMobile.js
@@ -8,7 +8,7 @@ export function handleMobile(e) {
   // update with new projects info
   const pictures = {
     project1: './dist/images/Queen-tribute-page.png',
-    project2: './dist/images/background3-mod.jpg',
+    project2: './dist/images/survey-form.png',
     project3: './dist/images/background3-mod.jpg',
     project4: './dist/images/background3-mod.jpg',
     project5: './dist/images/background3-mod.jpg',
@@ -18,7 +18,7 @@ export function handleMobile(e) {
   // update with new projects info
   const texts = {
     project1: 'This is a tribute page about Queen band. I prepared it as an exercise for FCC certification in Responsive Web Design. It was prepared with pure ES6 and SASS for styling. It is a quite simple desing but I tried to make it a bit more attractive.',
-    project2: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos maxime dolor ex, debitis reiciendis impedit cupiditate natus? Dolorem, accusamus sint quisquam dicta, accusantium labore provident alias natus deserunt quia facere nisi iusto? Aperiam odit, similique adipisci assumenda dignissimos iusto, tempora dolorum debitis excepturi ad at, a voluptatibus. Laborum, accusamus repellat.',
+    project2: 'This project is my approach to make a solid survey-form example with solid semantics and custom form validation with JS used. In the future there might be additional backend script added to handle data got from the form. Besides that, all functionality is fully prepared. Form is prepared with CSS grid usage. I also added custom feel and look to inputs.',
     project3: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos maxime dolor ex, debitis reiciendis impedit cupiditate natus? Dolorem, accusamus sint quisquam dicta, accusantium labore provident alias natus deserunt quia facere nisi iusto? Aperiam odit, similique adipisci assumenda dignissimos iusto, tempora dolorum debitis excepturi ad at, a voluptatibus. Laborum, accusamus repellat.',
     project4: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos maxime dolor ex, debitis reiciendis impedit cupiditate natus? Dolorem, accusamus sint quisquam dicta, accusantium labore provident alias natus deserunt quia facere nisi iusto? Aperiam odit, similique adipisci assumenda dignissimos iusto, tempora dolorum debitis excepturi ad at, a voluptatibus. Laborum, accusamus repellat.',
     project5: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos maxime dolor ex, debitis reiciendis impedit cupiditate natus? Dolorem, accusamus sint quisquam dicta, accusantium labore provident alias natus deserunt quia facere nisi iusto? Aperiam odit, similique adipisci assumenda dignissimos iusto, tempora dolorum debitis excepturi ad at, a voluptatibus. Laborum, accusamus repellat.',
@@ -28,7 +28,7 @@ export function handleMobile(e) {
   // update with new projects info
   const links = {
     project1: 'https://github.com/RXstoinkboy/Tribute-page',
-    project2: 'https://github.com/RXstoinkboy/Tribute-page',
+    project2: 'https://github.com/RXstoinkboy/survey-form',
     project3: 'https://github.com/RXstoinkboy/Tribute-page',
     project4: 'https://github.com/RXstoinkboy/Tribute-page',
     project5: 'https://github.com/RXstoinkboy/Tribute-page',
